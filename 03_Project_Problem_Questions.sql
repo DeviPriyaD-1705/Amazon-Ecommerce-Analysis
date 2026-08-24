4d4bfd96c0d3d87ff707caf2ed947a2b6a515ccc
Devi Priya D <devidurai1720@gmail.com>
2026-08-25 00:16:20 +0530

    Add project business questions

---
+++ 03_Project_Problem_Questions.sql
@@ -0,0 +1,104 @@
+-- =========================================
+-- BUSINESS PROBLEMS
+-- =========================================
+
+
+/*
+ 1. Best-Performing Products
+ Identify the 10 products that generated the highest sales revenue.
+ Show the product name, total quantity sold, and total sales generated.
+*/
+
+
+/*
+2. Revenue Contribution by Category
+Find out how much revenue each product category generates
+and what percentage of the overall revenue comes from each category.
+*/
+
+
+/*
+3. Average Order Value (AOV)
+Compute the average order value for each customer.
+Include only customers with more than 5 orders.
+*/
+
+
+/*
+4. Customers with No Purchases
+Find customers who have registered but never placed an order.
+List customer details.
+*/
+
+
+/*
+5. Best-Selling Categories by State
+Identify the best-selling product category for each state.
+Include the total sales for that category within each state.
+*/
+
+
+/*
+6. Customer Lifetime Value (CLTV)
+Calculate the total value of orders placed by each customer over their lifetime. 
+Rank customers based on their CLTV
+*/
+
+
+/*
+7. Inventory Stock Alerts 
+Query products with stock levels below a certain threshold (e.g., less than 10 units). 
+Include last restock date and warehouse information.
+*/
+
+
+/*
+8. Shipping Delays
+Identify orders where the shipping date is later than 3 days after the order date.
+Sort the results by shipping delay, from highest to lowest.
+Include customer, order details, and delivery provider.
+*/
+
+
+/*
+9. Payment Success Rate 
+
+Calculate the percentage of successful payments across all orders.
+Include breakdowns by payment status (e.g., failed, pending).
+*/
+
+
+/*
+10. Product Profit Margin
+Calculate the profit margin for each product sold (difference between price and cost of goods sold).
+Rank products by their profit margin, showing highest to lowest.
+*/
+
+
+/*
+11. Most Returned Products
+Query the top 10 products by the number of returns.
+Display the return rate as a percentage of total units sold for each product.
+*/
+
+
+/*
+12. Top 5 customers by orders in each state
+Identify the top 5 customers with the highest number of orders by each state
+Include the number of orders and total sales for each customers
+*/
+
+
+/*
+13. Monthly Sales Trend
+Query monthly total sales over the past year.
+Display the sales trend, grouping by month, return current_month sale, last month sale
+*/
+
+
+
+/*
+14. Top Performing Sellers
+Find the top 5 sellers based on total sales value.
+Include both successful and failed orders, display their percentage of successful orders.
+*/
